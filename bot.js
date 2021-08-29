dd845b8cce8aaad66d6e093fc84c459343a42293
Alex <alexurban12@gmail.com>
2021-08-30 01:58:00 +0200

    trying another webhook method

--- bot.js
+++ bot.js
@@ -12,13 +12,13 @@ const apiurl = process.env.TOKEN_API;
 const apiLPurl = process.env.LP_API;
 
 const PORT = process.env.PORT || 3000;
-const URL = 'https://kardia-info-bot.herokuapp.com/';
+const HERO_URL = 'https://kardia-info-bot.herokuapp.com/';
 
 let bot;
 if (process.env.NODE_ENV === 'production') {
     bot = new Telegraf(process.env.BOT_TOKEN);
-    bot.telegram.setWebhook(`${URL}/bot${process.env.BOT_TOKEN}`);
-    expressApp.use(bot.webhookCallback(`/bot${process.env.BOT_TOKEN}`));
+    //bot.telegram.setWebhook(`${HERO_URL}/bot${process.env.BOT_TOKEN}`);
+    //expressApp.use(bot.webhookCallback(`/bot${process.env.BOT_TOKEN}`));
     //bot.setWebHook(process.env.HEROKU_URL + bot.token);
  } else {
     bot = new Telegraf('1962673670:AAHtYB7Y1bW9zkpAuOQCR3qRmGeZthxIJSc');
@@ -708,12 +708,20 @@ function abbreviate(num, fixed) {
 
 
 if(process.env.NODE_ENV == 'production'){
-    expressApp.get('/', (req, res) => {
-        res.send(`Hello World!\nPort Number: ${PORT}`);
-      });
-      expressApp.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-      });
+    // expressApp.get('/', (req, res) => {
+    //     res.send(`Hello World!\nPort Number: ${PORT}`);
+    //   });
+    //   expressApp.listen(PORT, () => {
+    //     console.log(`Server running on port ${PORT}`);
+    //   });
+
+
+      bot.launch({
+        webhook: {
+          domain: HERO_URL,
+          port: PORT
+        }
+      })
 } else {
     bot.launch();
 }
