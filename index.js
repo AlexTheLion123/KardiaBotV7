81b14f31fcecf7860cd813adc97fb8820b6c8001
Alex <alexurban12@gmail.com>
2021-08-30 21:22:32 +0200

    change

--- index.js
+++ index.js
@@ -78,15 +78,7 @@ Here is the breakdown of commands that I support:
 
 To show the chart for a token, just find the token in the menu or send a message with the token name as a single word. Try it, type in 'kai'
 `
-WELCOME_MESSAGE = 
-`
-🚀 Welcome #${ctx.from.first_name} to ${ctx.chat.title}. I am the #KardiaInfo bot and my aim to keep you up to date with the latest information regarding Kardiachain.
 
-The list of commands I support are as follows:
-/price coin - e.g. /price beco
-/start - to display a button selection of all supported coins
-/list, /help, /info - to display a list of all supported coins
-`
 
 bot.command(["start","menu"], async (ctx) => {
     return mainMenu(ctx);
@@ -101,6 +93,15 @@ bot.command("help", async ctx=> {
 })
 
 bot.on('new_chat_members', async ctx => {
+    const WELCOME_MESSAGE = 
+`
+🚀 Welcome #${ctx.from.first_name} to ${ctx.chat.title}. I am the #KardiaInfo bot and my aim to keep you up to date with the latest information regarding Kardiachain.
+
+The list of commands I support are as follows:
+/price coin - e.g. /price beco
+/start - to display a button selection of all supported coins
+/list, /help, /info - to display a list of all supported coins
+`
     if(groupWhitelist.includes(ctx.chat.id)){
         return ctx.reply(WELCOME_MESSAGE, {parse_mode: 'markdown'})
     }
