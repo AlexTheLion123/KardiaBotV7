81d665c43de236e9c682a9999e0f095b4214fdc3
Alex <alexurban12@gmail.com>
2021-08-30 02:17:51 +0200

    Back to express

--- bot.js
+++ bot.js
@@ -710,6 +710,10 @@ function abbreviate(num, fixed) {
 if(process.env.NODE_ENV == 'production'){
     expressApp.get('/', (req, res) => {
         res.send(`Hello World!\nPort Number: ${PORT}`);
+
+        bot.start(ctx => {
+            return ctx.reply("Hello World!")
+        });
       });
       expressApp.listen(PORT, () => {
         console.log(`Server running on port ${PORT}`);
