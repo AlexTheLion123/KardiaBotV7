64e918b3f0b99cf9a4131cc644299dd1a6a0dbc1
Alex <alexurban12@gmail.com>
2021-08-30 18:04:47 +0200

    change

--- index.js
+++ index.js
@@ -157,6 +157,10 @@ fetch('https://kardia-info-backend.herokuapp.com/api/')
         bot.hears("Tokens", ctx => {
             displayKeyboard(ctx, res, `*Click on a coin*`);
         })
+
+        bot.hears("alsdkjalskfj", ctx => {
+            ctx.reply("asdfasfdsfkjasfh");
+        })
         
         bot.hears(coinlist, (ctx) =>{      
             output(ctx.message.text, ctx);   
