796ac379aa6163c780715f91dae66a47d33cece9
Alex <alexurban12@gmail.com>
2021-08-30 20:34:33 +0200

    change

--- index.js
+++ index.js
@@ -547,8 +547,9 @@ async function output(name, ctx){
             
             return chartlink;
         })
-        .then(async res=>{    
-            return await ctx.replyWithPhoto(res, 
+        .then(async res=>{ 
+            ctx.reply("hello")   
+            ctx.replyWithPhoto(res, 
                 {   
                     reply_to_message_id: ctx.message.message_id,
                     caption: replyMessage,
